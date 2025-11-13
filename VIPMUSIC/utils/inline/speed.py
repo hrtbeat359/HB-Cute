f14c503e658249ca8ab2178cc562255e929e22eb
GhosttBatt <ghosttbatt@gmail.com>
2025-11-13 18:04:09 +0530

    speed.py

--- VIPMUSIC/utils/inline/speed.py
+++ VIPMUSIC/utils/inline/speed.py
@@ -5,30 +5,11 @@ def speed_markup(_, chat_id):
     upl = InlineKeyboardMarkup(
         [
             [
-                InlineKeyboardButton(
-                    text="🕒 0.5x",
-                    callback_data=f"SpeedUP {chat_id}|0.5",
-                ),
-                InlineKeyboardButton(
-                    text="🕓 0.75x",
-                    callback_data=f"SpeedUP {chat_id}|0.75",
-                ),
-            ],
-            [
-                InlineKeyboardButton(
-                    text=_["P_B_4"],
-                    callback_data=f"SpeedUP {chat_id}|1.0",
-                ),
-            ],
-            [
-                InlineKeyboardButton(
-                    text="🕤 1.5x",
-                    callback_data=f"SpeedUP {chat_id}|1.5",
-                ),
-                InlineKeyboardButton(
-                    text="🕛 2.0x",
-                    callback_data=f"SpeedUP {chat_id}|2.0",
-                ),
+                InlineKeyboardButton(text=".5x", callback_data=f"SpeedUP {chat_id}|0.5"),
+                InlineKeyboardButton(text=".75x", callback_data=f"SpeedUP {chat_id}|0.75"),
+                InlineKeyboardButton(text=_["1x"], callback_data=f"SpeedUP {chat_id}|1.0"),
+                InlineKeyboardButton(text="1.5x", callback_data=f"SpeedUP {chat_id}|1.5"),
+                InlineKeyboardButton(text="2.0x", callback_data=f"SpeedUP {chat_id}|2.0"),
             ],
             #[
              #   InlineKeyboardButton(
