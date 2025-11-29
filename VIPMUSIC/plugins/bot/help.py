0d03d51e6a8d5c833c4c074fba5adf059b8be85d
hrtbeat359 <hrtbeat359@gmail.com>
2025-11-30 00:10:24 +0530

    help.py

--- VIPMUSIC/plugins/bot/help.py
+++ VIPMUSIC/plugins/bot/help.py
@@ -10,7 +10,11 @@ from VIPMUSIC.utils.inline.help import (
     first_page,
     private_help_panel,
     music_panel,
-    games_panel,
+    games_panel1,
+    games_panel2,
+    games_panel3,
+    games_panel4,
+    games_panel5,
     chat_panel,
     reaction_panel,
     mention_panel,
@@ -170,7 +174,7 @@ async def help_category_cb(client, CallbackQuery, _):
         return
 
     if cat == "games":
-        keyboard = games_panel(_)
+        keyboard = games_panel1(_)
         try:
             await CallbackQuery.message.edit_text(_["help_1"], reply_markup=keyboard)
         except:
@@ -216,6 +220,53 @@ async def help_category_cb(client, CallbackQuery, _):
 
     return await CallbackQuery.answer("Unknown", show_alert=True)
 
+#game panel callbacks
+@app.on_callback_query(filters.regex(r"games_p1|games_p2|games_p3|games_p4|games_p5") & ~BANNED_USERS)
+@languageCB
+async def games_paging_cb(client, CallbackQuery, _):
+    data = CallbackQuery.data.strip()
+
+    if data == "games_p1":
+        keyboard = games_panel1(_)
+        try:
+            await CallbackQuery.message.edit_text(_["help_1"], reply_markup=keyboard)
+        except:
+            pass
+        await CallbackQuery.answer()
+        return
+    if data == "games_p2":
+        keyboard = games_panel2(_)
+        try:
+            await CallbackQuery.message.edit_text(_["help_1"], reply_markup=keyboard)
+        except:
+            pass
+        await CallbackQuery.answer()
+        return
+    if data == "games_p3":
+        keyboard = games_panel3(_)
+        try:
+            await CallbackQuery.message.edit_text(_["help_1"], reply_markup=keyboard)
+        except:
+            pass
+        await CallbackQuery.answer()
+        return
+    if data == "games_p4":
+        keyboard = games_panel4(_)
+        try:
+            await CallbackQuery.message.edit_text(_["help_1"], reply_markup=keyboard)
+        except:
+            pass
+        await CallbackQuery.answer()
+        return
+    if data == "games_p5":
+        keyboard = games_panel5(_)
+        try:
+            await CallbackQuery.message.edit_text(_["help_1"], reply_markup=keyboard)
+        except:
+            pass
+        await CallbackQuery.answer()
+        return
+        
 
 # Management paging callbacks: management_p1, management_p2, management_p3
 @app.on_callback_query(filters.regex(r"management_p1|management_p2|management_p3") & ~BANNED_USERS)
