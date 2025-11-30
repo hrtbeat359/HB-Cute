47881b10f176cff22f586f7bbcd0d5b3d8c5131d
hrtbeat359 <hrtbeat359@gmail.com>
2025-11-30 05:39:18 +0530

    help.py

--- VIPMUSIC/plugins/bot/help.py
+++ VIPMUSIC/plugins/bot/help.py
@@ -220,6 +220,7 @@ async def help_category_cb(client, CallbackQuery, _):
 
     return await CallbackQuery.answer("Unknown", show_alert=True)
 
+"""
 #game panel callbacks
 @app.on_callback_query(filters.regex(r"games_p1|games_p2|games_p3|games_p4|games_p5") & ~BANNED_USERS)
 @languageCB
@@ -271,7 +272,7 @@ async def games_paging_cb(client, CallbackQuery, _):
         await CallbackQuery.answer()
         return
         
-
+"""
 # Management paging callbacks: management_p1, management_p2, management_p3
 @app.on_callback_query(filters.regex(r"management_p1|management_p2|management_p3") & ~BANNED_USERS)
 @languageCB
