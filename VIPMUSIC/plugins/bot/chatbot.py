66dc42d08c4e897fbc915b821243d88eb855f305
hrtbeat359 <hrtbeat359@gmail.com>
2025-12-01 11:09:28 +0530

    chatbot.py

--- VIPMUSIC/plugins/bot/chatbot.py
+++ VIPMUSIC/plugins/bot/chatbot.py
@@ -55,24 +55,19 @@ message_counts = {}
 
 # ================= DEFAULT BLOCKED WORDS ================= #
 DEFAULT_BLOCKED = [
-    # English sexual content
     "sex", "porn", "nude", "boob", "boobs", "dick", "cock", "penis", "vagina",
     "nipples", "xxx", "porno", "cum", "masturbate", "erotic", "adult", "playboy",
     "hentai", "erotica", "fetish", "kink", "orgasm", "threesome", "xnxx", "xvideos", "xvideo", "pic", "nudepic",
     
-    # Tamil transliterated sexual words (examples)
     "punda", "koothi", "soothu", "sutthu", "mayiru", "olmari", "okka", 
     "poolu", "olu", "sappu", "umbe", "kuththu", "thappu","suthu", "paalu",
     "Adangommala", "Adangomala", "adangotha", "adangottha",
     
-    # Common bypass words or slang
     "sunny", "call", "pm", "dm", "service", "ottha", "otta", "gommala",
     "hole", "inch", "ash", "sexchat", "onlyfans", "cams", "chatsex", "adultchat",
     "videochat", "sexting", "naked", "fetish", "lingerie", "eroticvideo",
     
-    # Emojis often used in sexual context
-    "💦", "💧", "🍑", "🍒", "🍆", "🥵",  "🍌", "💋",
-     "👅", 
+    "💦", "💧", "🍑", "🍒", "🍆", "🥵",  "🍌", "💋", "👅", 
 ]
 
 # ===================== INITIALIZE BLOCKLIST ===================== #
